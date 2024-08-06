7923ecb2b68d95699a2be7436a5e8beb2b1844dc
Eli <78963198+niceEli@users.noreply.github.com>
2024-08-06 19:24:25 -0400

    feat: more backup fonts

--- lua/options.lua
+++ lua/options.lua
@@ -6,5 +6,5 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font Mono,Consolas:h14" -- text below applies for VimScript
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono,JetBrainsMono Nerd Font,Consolas:h14" -- text below applies for VimScript
 end
