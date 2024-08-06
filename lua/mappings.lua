f62a0a4a97f9c15e35d3746ce35f1353b1bef126
Eli <78963198+niceEli@users.noreply.github.com>
2024-08-06 19:14:18 -0400

    chore: mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -44,13 +44,20 @@ map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
 -- Magic buffer-picking mode
 map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
 -- Sort automatically by...
-map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts)
-map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+map('n', '<Space>Bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>Bn', '<Cmd>BufferOrderByName<CR>', opts)
+map('n', '<Space>Bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>Bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>Bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 
 -- Other:
 -- :BarbarEnable - enables barbar (enabled by default)
 -- :BarbarDisable - very bad command, should never be used
+
+-- copilot using enter key
+
+vim.keymap.set('i', '<Right>', 'copilot#Accept("\\<Right>")', {
+  expr = true,
+  replace_keycodes = false
+})
 
