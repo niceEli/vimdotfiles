7f70137cfa7ca41c51f4e3a9f4dc9328a6ce0f99
Eli <78963198+niceEli@users.noreply.github.com>
2024-08-06 21:04:04 -0400

    feat: markdown preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -42,6 +42,26 @@ return {
     lazy = false
   },
 
+  {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    keys = {
+      {
+        "<leader>op",
+          function()
+            local peek = require("peek")
+              if peek.is_open() then
+            peek.close()
+            else
+            peek.open()
+            end
+        end,
+        desc = "Peek (Markdown Preview)",
+      },
+  },
+    opts = { theme = "dark", app = "browser" },
+  },
+
   "NvChad/nvcommunity",
   { import = "nvcommunity.git.diffview" },
   { import = "nvcommunity.git.neogit" },
