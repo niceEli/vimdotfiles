dd693a6cdf0ab25bf2abbc110ac79ab247adff19
Eli <78963198+niceEli@users.noreply.github.com>
2024-08-08 15:26:21 -0400

    Revert "Merge branch 'NvChad:main' into main"
    This reverts commit 28a184a31d900d8ba96639d2264311d174d1ed30, reversing
    changes made to 0f075cd1b6842fe535822dc0c24d36f44cb15348.

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ vim.wo.relativenumber = true -- line numbers
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if not vim.uv.fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
@@ -22,6 +22,9 @@ require("lazy").setup({
     lazy = false,
     branch = "v2.5",
     import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
   },
 
   { import = "plugins" },
@@ -31,7 +34,6 @@ require("lazy").setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
-require "options"
 require "nvchad.autocmds"
 
 vim.schedule(function()
