2d8c1e366348952f7317aba1cb0f1fc353054135
Eli <78963198+niceEli@users.noreply.github.com>
2024-08-08 15:42:12 -0400

    fix: oops

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,6 +39,8 @@ return {
     "ellisonleao/carbon-now.nvim",
     lazy = true,
     cmd = "CarbonNow",
+    ---@param opts cn.ConfigSchema
+    opts = { [[ your custom config here ]] }
   },
   
   {
