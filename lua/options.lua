675ded663e831ddf16fa1c8ac476d2b25dcfd933
Eli <78963198+niceEli@users.noreply.github.com>
2024-08-11 18:32:34 -0400

    feat: rulers

--- lua/options.lua
+++ lua/options.lua
@@ -8,3 +8,6 @@ require "nvchad.options"
 if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono Nerd Font Mono,JetBrainsMono Nerd Font,Consolas:h14" -- text below applies for VimScript
 end
+
+-- Set color columns at 72, 80, and 120 characters
+vim.opt.colorcolumn = "72,80,120"
